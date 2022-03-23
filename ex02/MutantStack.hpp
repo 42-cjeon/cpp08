8da9191a4b57da290f90f2943166ede4eb3424d7
anduuin <jcm2232@gmail.com>
2022-03-23 13:44:04 +0900

    Implement ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 14:32:45 by cjeon             #+#    #+#             */
-/*   Updated: 2022/03/22 20:37:51 by cjeon            ###   ########.fr       */
+/*   Updated: 2022/03/23 13:37:41 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,50 @@
 template <typename T, typename Container = std::deque<T> >
 class MutantStack {
  public:
-  explicit MutantStack(const Container &cont = Container()) : c(cont) {}
+  typedef T value_type;
+  typedef typename Container::allocator_type allocator_type;
+  typedef typename Container::size_type size_type;
+  typedef typename Container::difference_type difference_type;
+  typedef T &reference;
+  typedef const T &const_reference;
+  typedef typename Container::pointer pointer;
+  typedef typename Container::const_pointer const_pointer;
+  typedef typename Container::iterator iterator;
+  typedef typename Container::const_iterator const_iterator;
+  typedef typename Container::reverse_iterator reverse_iterator;
+  typedef typename Container::const_reverse_iterator const_reverse_iterator;
+
+  explicit MutantStack(const Container &cont = Container());
   MutantStack(const MutantStack &other) : c(other.c) {}
   ~MutantStack(void) {}
-  MutantStack &operator=(const MutantStack &other) { c = other.c; }
+  MutantStack &operator=(const MutantStack &other) {
+    c = other.c;
+    return *this;
+  }
 
   T &top(void) { return c.back(); }
   const T &top(void) const { return c.back(); }
 
   bool empty(void) const { return c.empty(); }
-  typename Container::size_type size(void) const { return c.size(); }
+  size_type size(void) const { return c.size(); }
 
   void push(const T &value) { c.push_back(value); }
   void pop(void) { c.pop_back(); }
 
-  typename Container::iterator begin() { return c.begin(); }
-  typename Container::const_iterator begin() const { return c.begin(); }
-  typename Container::iterator end() { return c.end(); }
-  typename Container::const_iterator end() const { return c.end(); }
-  typename Container::iterator rbegin() { return c.rbegin(); }
-  typename Container::const_reverse_iterator rbegin() const { return c.rbegin(); }
-  typename Container::iterator rend() { return c.rend(); }
-  typename Container::const_reverse_iterator rend() const { return c.rend(); }
+  iterator begin() { return c.begin(); }
+  const_iterator begin() const { return c.begin(); }
+  iterator end() { return c.end(); }
+  const_iterator end() const { return c.end(); }
+  reverse_iterator rbegin() { return c.rbegin(); }
+  const_reverse_iterator rbegin() const { return c.rbegin(); }
+  reverse_iterator rend() { return c.rend(); }
+  const_reverse_iterator rend() const { return c.rend(); }
 
  protected:
   Container c;
 };
 
+/*
 template <typename T, typename Container>
 bool operator==(const MutantStack<T, Container> &lhs,
                 const MutantStack<T, Container> &rhs) {
@@ -80,5 +97,6 @@ bool operator>=(const MutantStack<T, Container> &lhs,
                 const MutantStack<T, Container> &rhs) {
   return lhs.c >= rhs.c;
 }
+*/
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,25 +6,121 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 14:32:53 by cjeon             #+#    #+#             */
-/*   Updated: 2022/03/22 20:41:02 by cjeon            ###   ########.fr       */
+/*   Updated: 2022/03/23 13:40:30 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "MutantStack.hpp"
-#include <stack>
-
-//#define MutantStack std::stack
+#include <vector>
 
 int main(void) {
-  MutantStack<int> ms;
+  std::cout.setf(std::ios::boolalpha);
+  std::cout << "--- basic test ---" << std::endl;
+  {
+    MutantStack<int> ms;
+    std::cout << "size: " << ms.size() << std::endl;
+    std::cout << "empty? : " << ms.empty() << std::endl;
 
-  for (int i = 0; i < 10; ++i) {
-    ms.push(i);
+    for (int i = 0; i < 10; ++i) {
+      std::cout << "Add " << i << std::endl;
+      ms.push(i);
+    }
+    std::cout << "size: " << ms.size() << std::endl;
+    std::cout << "empty? : " << ms.empty() << std::endl;
+
+    for (int i = 0; i < 10; ++i) {
+      std::cout << "top: " << ms.top() << std::endl;
+      std::cout << "pop" << std::endl;
+      ms.pop();
+    }
+    std::cout << "size: " << ms.size() << std::endl;
+    std::cout << "empty? : " << ms.empty() << std::endl;
   }
+  std::cout << "--- iterator test ---" << std::endl;
+  {
+    MutantStack<int> ms;
+    for (int i = 0; i < 10; ++i) {
+      ms.push(i);
+    }
+    std::cout << ":ITERATOR:" << std::endl;
+    for (MutantStack<int>::iterator it = ms.begin(); it != ms.end(); ++it) {
+      std::cout << *it << std::endl;
+    }
+    std::cout << ":REVERSE-ITERATOR:" << std::endl;
+    for (MutantStack<int>::reverse_iterator it = ms.rbegin(); it != ms.rend(); ++it) {
+      std::cout << *it << std::endl;
+    }
+  }
+  std::cout << "--- copy ctor test ---" << std::endl;
+  {
+    MutantStack<int> ms;
+    for (int i = 0; i < 10; ++i) {
+      ms.push(i);
+    }
+    MutantStack<int> ms2(ms);
+    for (MutantStack<int>::iterator it = ms2.begin(); it != ms2.end(); ++it) {
+      std::cout << *it << std::endl;
+    }
+  }
+  std::cout << "--- assignment operator test --" << std::endl;
+  {
+    MutantStack<int> ms;
+    for (int i = 0; i < 10; ++i) {
+      ms.push(i);
+    }
 
-  for (MutantStack<int>::iterator) {
-    std::cout << ms.top() << std::endl;
-    ms.pop();
+    MutantStack<int> ms2;
+    ms2 = ms;
+    for (MutantStack<int>::iterator it = ms2.begin(); it != ms2.end(); ++it) {
+      std::cout << *it << std::endl;
+    }
+  }
+  std::cout << "--- compounded type test (deep copy) --" << std::endl;
+  {
+    MutantStack<std::string> ms;
+    std::string str("");
+    for (int i = 0; i < 10; ++i) {
+      str += "-";
+      ms.push(str);
+    }
+    MutantStack<std::string> ms2(ms);
+    for (int i = 0; i < 10; ++i) {
+      ms2.top() = "-";
+      ms2.pop();
+    }
+    for (int i = 0; i < 10; ++i) {
+      ms2.push(str.substr(0, 10 - i));
+    }
+    for (MutantStack<std::string>::iterator it = ms.begin(); it != ms.end(); ++it) {
+      std::cout << *it << std::endl;
+    }
+    for (MutantStack<std::string>::iterator it = ms2.begin(); it != ms2.end(); ++it) {
+      std::cout << *it << std::endl;
+    }
+  }
+  std::cout << "--- some other container ---" << std::endl;
+  {
+    typedef MutantStack<std::string, std::vector<std::string> >  MutantMutantStack;
+    MutantMutantStack ms;
+    std::string str("");
+    for (int i = 0; i < 10; ++i) {
+      str += "-";
+      ms.push(str);
+    }
+    MutantMutantStack ms2(ms);
+    for (int i = 0; i < 10; ++i) {
+      ms2.top() = "-";
+      ms2.pop();
+    }
+    for (int i = 0; i < 10; ++i) {
+      ms2.push(str.substr(0, 10 - i));
+    }
+    for (MutantMutantStack::iterator it = ms.begin(); it != ms.end(); ++it) {
+      std::cout << *it << std::endl;
+    }
+    for (MutantMutantStack::iterator it = ms2.begin(); it != ms2.end(); ++it) {
+      std::cout << *it << std::endl;
+    }
   }
 }
